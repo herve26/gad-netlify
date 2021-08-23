0e38e584c3e35cf602c3895b1d73e52b6011f1db
herve26 <nzanzuh@gmail.com>
2021-08-23 01:16:30 +0100

    changed style file type to scss

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,49 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+
+import { Header } from "@components/Organisms";
+import { Main, Section, Hero } from "@components/Molecules";
+import Footer from "@components/Footer";
+
+import styles from "./Home.module.scss";
+
+import info from "../config/info";
+
+const homeConfig = {
+  heroUrl:
+    "https://storage.googleapis.com/gad-static-image/teemu-paananen-rd5uNIUJCF0-unsplash%20(Moyen)_1151x496.jpg"
+};
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>{"site_title"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
+      <Header />
+      <Main>
+        <Section>
+          <Hero src={homeConfig.heroUrl}>
+            <h2 className={styles.hero_text_main}>
+              <span
+                className={`${styles.hero_text_block} ${styles.hero_bg} text-alt`}
+              >
+                Empire
+              </span>
+              <br />
+              <span className={`${styles.hero_text_block} text-alt`}>
+                Of Tomorrow
+              </span>
+            </h2>
+            <h3 className={`${styles.hero_text_side} text-alt`}>
+              <span>Let's Build a better Afica</span>
+              <br />
+              <span>And a better World</span>
+            </h3>
+          </Hero>
+        </Section>
+      </Main>
       <Footer />
-    </div>
-  )
+    </>
+  );
 }
